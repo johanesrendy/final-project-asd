6f8555a902a86090f2307891ca424a3b952b50a0
johanesrendy <johanesrendy9@gmail.com>
2023-12-15 12:46:45 +0700

    update sudoku

--- src/sudoku/SudokuMain.java
+++ src/sudoku/SudokuMain.java
@@ -1,35 +1,45 @@
 package sudoku;
+
 import java.awt.*;
 import javax.swing.*;
+
 /**
  * The main Sudoku program
  */
 public class SudokuMain extends JFrame {
-   private static final long serialVersionUID = 1L;  // to prevent serial warning
+    private static final long serialVersionUID = 1L; // to prevent serial warning
 
-   // private variables
-   GameBoardPanel board = new GameBoardPanel();
-   JButton btnNewGame = new JButton("New Game");
+    // private variables
+    GameBoardPanel board = new GameBoardPanel();
+    JButton btnNewGame = new JButton("New Game");
 
-   // Constructor
-   public SudokuMain() {
-      Container cp = getContentPane();
-      cp.setLayout(new BorderLayout());
+    // Constructor
+    public SudokuMain() {
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
 
-      cp.add(board, BorderLayout.CENTER);
+        cp.add(board, BorderLayout.CENTER);
 
-      // Add a button to the south to re-start the game via board.newGame()
+        // Add a button to the south to re-start the game via board.newGame()
+        JButton btnNewGame = new JButton("New Game");
+        btnNewGame.addActionListener(e -> board.newGame());
+        cp.add(btnNewGame, BorderLayout.SOUTH);
 
-      // Initialize the game board to start the game
-      board.newGame();
+        // Initialize the game board to start the game
+        board.newGame();
 
-      pack();     // Pack the UI components, instead of using setSize()
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // to handle window-closing
-      setTitle("Sudoku");
-      setVisible(true);
-   }
+        pack(); // Pack the UI components, instead of using setSize()
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // to handle window-closing
+        setTitle("Sudoku");
+        setVisible(true);
+    }
 
-   /** The entry main() entry method */
-   public static void main(String[] args) {
-   }
+    /** The entry main() entry method */
+    public static void main(String[] args) {
+        // [TODO 1] Check "Swing program template" on how to run
+        // the constructor of "SudokuMain"
+        SwingUtilities.invokeLater(() -> {
+            new SudokuMain();
+        });
+    }
 }
